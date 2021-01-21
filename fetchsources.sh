60eca59f501d840d8e143d78685f9a125b86d988
River Zhou <riverzhou2000@163.com>
2021-01-22 06:46:12 +0800

    Add: Script for fetch remote repo

---
+++ fetchsources.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#git fetch  github chromeos-5.4
+#git fetch  github main
+
+git fetch  android android11-5.4-lts
+git fetch  cloudready main
+git fetch  google chromeos-5.4
+git fetch  ubuntu master
+
+
