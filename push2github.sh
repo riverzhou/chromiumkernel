d7b26266681da75eb94bcd1a9c03a10ead43bab8
River Zhou <riverzhou2000@163.com>
2021-01-22 06:56:48 +0800

    Add: Script for push code to github

---
+++ push2github.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git push github remotes/ubuntu/master:ubuntu
+git push github remotes/cloudready/main:cloudready
+git push github remotes/android/android11-5.4-lts:android
+git push github remotes/google/chromeos-5.4:google
+git push github main:main
+git push github chromeos-5.4:chromeos-5.4
+
+
