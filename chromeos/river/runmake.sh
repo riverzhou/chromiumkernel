d130344e0c42158144f8a5377c7aa9b7f4b093b1
River Zhou <riverzhou2000@163.com>
2021-01-22 08:10:01 +0800

    Bugfix: build script

--- chromeos/river/runmake.sh
+++ chromeos/river/runmake.sh
@@ -5,5 +5,5 @@ rm ../install -rf
 mkdir ../install
 make CC=clang LLVM=1 O=build -j8 
 cp build/arch/x86/boot/bzImage ../install/kernel-river 
-make CC=clang LLVM=1 O=build -j8 modules_install INSTALL_MOD_PATH=../install/
+make CC=clang LLVM=1 O=build -j8 modules_install INSTALL_MOD_PATH=../../install/
 
