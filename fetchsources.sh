4e156afaded1136b83bd312b5f16737aa59fe9ab
River Zhou <riverzhou2000@163.com>
2021-01-22 11:55:14 +0800

    Update: add microsoft 5.4 kernel

--- fetchsources.sh
+++ fetchsources.sh
@@ -7,5 +7,5 @@ git fetch  android android11-5.4-lts
 git fetch  cloudready main
 git fetch  google chromeos-5.4
 git fetch  ubuntu master
-
+git fetch  microsoft linux-msft-wsl-5.4.y
 
--- push2github.sh
+++ push2github.sh
@@ -4,7 +4,7 @@ git push github remotes/ubuntu/master:ubuntu
 git push github remotes/cloudready/main:cloudready
 git push github remotes/android/android11-5.4-lts:android
 git push github remotes/google/chromeos-5.4:google
+git push github linux-msft-wsl-5.4.y:microsoft
 git push github main:main
-git push github chromeos-5.4:chromeos-5.4
-
+#git push github chromeos-5.4:chromeos-5.4
 
