d3abd8391b0d5b3f0358286167e5f7b53355ddeb
River Zhou <riverzhou2000@163.com>
2021-02-11 07:01:52 +0800

    Add android12-5.10 branch

--- fetchsources.sh
+++ fetchsources.sh
@@ -4,6 +4,7 @@
 #git fetch  github main
 
 git fetch  android android11-5.4-lts
+git fetch  android android12-5.10
 git fetch  cloudready main
 git fetch  google chromeos-5.4
 git fetch  google chromeos-5.10
