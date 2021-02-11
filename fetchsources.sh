98f3423debd9433d5d5bb7e46b3c179094696d73
River Zhou <riverzhou2000@163.com>
2021-02-11 11:53:20 +0800

    Add branch release-R89-13729.B-chromeos-5.4

--- fetchsources.sh
+++ fetchsources.sh
@@ -10,6 +10,7 @@ git fetch  google chromeos-5.4
 git fetch  google chromeos-5.10
 git fetch  google release-R89-13729.B-chromeos-5.10
 git fetch  google release-R87-13505.B-chromeos-5.4
+git fetch  google release-R89-13729.B-chromeos-5.4
 git fetch  ubuntu master
 git fetch  microsoft linux-msft-wsl-5.4.y
 
--- push2github.sh
+++ push2github.sh
@@ -8,6 +8,7 @@ git push github refs/remotes/google/chromeos-5.4:refs/heads/google
 git push github refs/remotes/google/chromeos-5.10:refs/heads/google-5.10
 git push github refs/remotes/google/release-R89-13729.B-chromeos-5.10:refs/heads/google-5.10-R89
 git push github refs/remotes/google/release-R87-13505.B-chromeos-5.4:refs/heads/google-5.4-R87
+git push github refs/remotes/google/release-R89-13729.B-chromeos-5.4:refs/heads/google-5.4-R89
 git push github refs/remotes/microsoft/linux-msft-wsl-5.4.y:refs/heads/microsoft
 git push github main:main
 
