03545c830a5e9269c0dff38fbbbe24ff2360cbbc
River Zhou <riverzhou2000@163.com>
2021-04-19 12:36:29 +0800

    clean branch

--- fetchsources.sh
+++ fetchsources.sh
@@ -5,12 +5,7 @@
 
 git fetch  android android11-5.4-lts
 git fetch  android android12-5.10
-git fetch  cloudready main
 git fetch  google chromeos-5.4
 git fetch  google chromeos-5.10
-git fetch  google release-R89-13729.B-chromeos-5.10
-git fetch  google release-R87-13505.B-chromeos-5.4
-git fetch  google release-R89-13729.B-chromeos-5.4
 git fetch  ubuntu master
-git fetch  microsoft linux-msft-wsl-5.4.y
 
